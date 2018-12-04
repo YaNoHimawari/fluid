9ca559019c251497d74648927e8fe9351e440773
Kaze <1468037166@qq.com>
2018-12-04 18:07:40 +0800

    刚体模拟实现

---
+++ assets/scripts/scene_chapters.js
@@ -0,0 +1,53 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    // update (dt) {},
+
+    //返回主页面
+    toMenu: function(){
+        cc.director.loadScene("MainMenu");
+    },
+
+    //跳转至指定关卡
+    toChapter: function(event, customEventData){
+        var scenename = "Chapter" + customEventData.toString();
+        console.log(scenename);
+        cc.director.loadScene(scenename);
+    },
+});
